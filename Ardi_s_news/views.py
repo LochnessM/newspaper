5c63470bfb5b894dbdd3a6b69d102570026fc931
LochnessM <klevis_ymeri@outlook.com>
2020-12-26 19:21:48 +0100

    u shtua appi artikulli

--- Ardi_s_news/views.py
+++ Ardi_s_news/views.py
@@ -4,6 +4,6 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'Faqja_kryesore/home.html')
     
 
---
+++ artikulli/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ArtikulliConfig(AppConfig):
+    name = 'artikulli'
