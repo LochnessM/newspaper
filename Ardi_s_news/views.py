9120e5de2c1ff8373d961a7ed0afb07b926c59bd
LochnessM <klevis_ymeri@outlook.com>
2020-12-27 19:29:25 +0100

    modeli i artikullit

--- Ardi_s_news/views.py
+++ Ardi_s_news/views.py
@@ -2,8 +2,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+sections = ['Aktualitet', 'Politike', 'Ekonomi', 'Opinion', 'Sporti', 'Kulture'] 
 
 def home(request):
-    return render(request, 'Faqja_kryesore/home.html')
+    return render(request, 'Faqja_kryesore/home.html', {'sections': sections, })
     
 
--- artikulli/models.py
+++ artikulli/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class artikulli(models.Model):
+    zgjedhje_e_kategorise = [('Aktualitet', 'Aktualitet'),
+                            ('Politike', 'Politike'),
+                            ('Ekonomi', 'Ekonomi'), 
+                            ('Opinion', 'Opinion'),
+                            ('Sporti', 'Sporti'),
+                            ('Kulture', 'Kulture')]
+    author = models.CharField(max_length=25)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    kategoria = models.CharField(max_length=10, choices=zgjedhje_e_kategorise, blank=True) 
+    data = models.DateField(auto_now_add=True)
+    image = models.ImageField(upload_to='artikulli/images/', blank=True)
