d491e73e81548ef86a77edbb5074f078d94eed27
LochnessM <klevis_ymeri@outlook.com>
2020-12-28 01:44:18 +0100

    seksioni i artikujve

--- Ardi_s_news/views.py
+++ Ardi_s_news/views.py
@@ -1,10 +1,12 @@
 
 from django.http import HttpResponse
 from django.shortcuts import render
+from artikulli.models import artikulli
 
 sections = ['Aktualitet', 'Politike', 'Ekonomi', 'Opinion', 'Sporti', 'Kulture'] 
 
 def home(request):
-    return render(request, 'Faqja_kryesore/home.html', {'sections': sections, })
+    artikujt = artikulli.objects.all()
+    return render(request, 'Faqja_kryesore/home.html', {'sections': sections, 'artikujt': artikujt})
     
 
