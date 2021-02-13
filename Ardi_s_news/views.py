2f5f5e8eea1ad3a807af2fccd27942592958fc10
LochnessM <klevis_ymeri@outlook.com>
2021-02-13 21:23:05 +0100

    shtoj ajax

--- Ardi_s_news/views.py
+++ Ardi_s_news/views.py
@@ -4,40 +4,64 @@ from django.shortcuts import render
 from artikulli.models import artikulli
 from django.core.paginator import Paginator
 
+artikujt_a = artikulli.objects.all().order_by('-data')
+paginator_aktualitet = Paginator(artikujt_a, 1)
 
-def home(request):
-    artikujt_aktualitet = artikulli.objects.all().order_by('-data')
-    paginator_aktualitet = Paginator(artikujt_aktualitet, 8)
-    aktualitet_page_num = request.GET.get('aktualitet_page', 1)
-    artikujt_aktualitet = paginator_aktualitet.page(aktualitet_page_num)
+artikujt_p = artikulli.objects.filter(kategoria = 'Politike').order_by('-data')
+paginator_politike = Paginator(artikujt_p, 1)
 
-    artikujt_politike = artikulli.objects.filter(kategoria = 'Politike').order_by('-data')
-    paginator_politike = Paginator(artikujt_politike, 8)
-    politike_page_num = request.GET.get('politike_page', 1)
-    artikujt_politike = paginator_politike.page(politike_page_num)
+artikujt_e = artikulli.objects.filter(kategoria = 'Ekonomi').order_by('-data')
+paginator_ekonomi = Paginator(artikujt_e, 1)
 
-    artikujt_ekonomi = artikulli.objects.filter(kategoria = 'Ekonomi').order_by('-data')
-    paginator_ekonomi = Paginator(artikujt_ekonomi, 8)
-    ekonomi_page_num = request.GET.get('ekonomi_page', 1)
-    artikujt_ekonomi = paginator_ekonomi.page(ekonomi_page_num)
+artikujt_o = artikulli.objects.filter(kategoria = 'Opinion').order_by('-data')
+paginator_opinion = Paginator(artikujt_o, 1)
 
-    artikujt_opinion = artikulli.objects.filter(kategoria = 'Opinion').order_by('-data')
-    paginator_opinion = Paginator(artikujt_opinion, 8)
-    opinion_page_num = request.GET.get('opinion_page', 1)
-    artikujt_opinion = paginator_opinion.page(opinion_page_num)
+artikujt_s = artikulli.objects.filter(kategoria = 'Sporti').order_by('-data')
+paginator_sporti = Paginator(artikujt_s, 1)
 
-    artikujt_sporti = artikulli.objects.filter(kategoria = 'Sporti').order_by('-data')
-    paginator_sporti = Paginator(artikujt_sporti, 8)
-    sporti_page_num = request.GET.get('sporti_page', 1)
-    artikujt_sporti = paginator_sporti.page(sporti_page_num)
+artikujt_k = artikulli.objects.filter(kategoria = 'Kulture').order_by('-data')
+paginator_kulture = Paginator(artikujt_k, 1)
 
-    artikujt_kulture = artikulli.objects.filter(kategoria = 'Kulture').order_by('-data')
-    paginator_kulture = Paginator(artikujt_kulture, 8)
-    kulture_page_num = request.GET.get('kulture_page', 1)
-    artikujt_kulture = paginator_kulture.page(kulture_page_num)
 
-    sections = {'Aktualitet': artikujt_aktualitet, 'Politike': artikujt_politike, 'Ekonomi': artikujt_ekonomi, 'Opinion': artikujt_opinion, 'Sporti': artikujt_sporti, 'Kulture': artikujt_kulture}
+def home(request):    
+    aktualitet = load_aktualitet(request)
+    politike = load_politike(request)
+    ekonomi = load_ekonomi(request)
+    opinion = load_opinion(request)
+    sporti = load_sporti(request)
+    kulture = load_kulture(request)
+
+    sections = {'Aktualitet': aktualitet, 
+                'Politike': politike, 
+                'Ekonomi': ekonomi, 
+                'Opinion': opinion, 
+                'Sporti': sporti, 
+                'Kulture': kulture}
 
     return render(request, 'Faqja_kryesore/home.html', {'sections': sections})
     
 
+
+def load_aktualitet(request):
+    page_num = request.GET.get('Aktualitet_page', 1)
+    return paginator_aktualitet.page(page_num)
+
+def load_politike(request):
+    page_num = request.GET.get('Politike_page', 1)
+    return paginator_politike.page(page_num)
+
+def load_ekonomi(request):
+    page_num = request.GET.get('Ekonomi_page', 1)
+    return paginator_ekonomi.page(page_num)
+
+def load_opinion(request):
+    page_num = request.GET.get('Opinion_page', 1)
+    return paginator_opinion.page(page_num)
+
+def load_sporti(request):
+    page_num = request.GET.get('Sporti_page', 1)
+    return paginator_sporti.page(page_num)
+
+def load_kulture(request):
+    page_num = request.GET.get('Kulture_page', 1)
+    return paginator_kulture.page(page_num)
